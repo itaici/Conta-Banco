2ccb56c001096358ab9214a820ed232895943de1
itaici <iplessmann@terra.com.br>
2024-05-07 15:31:47 -0300

    cricao processo conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tc = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        Double saldoConta;
+
+        System.out.println("Por favor, digite seu nome!");
+        nomeCliente = tc.nextLine();
+        System.out.println("Por favor, digite sua agencia!");
+        agencia = tc.nextLine();
+        System.out.println("Por favor, digite sua conta!");
+        numero = tc.nextInt();
+        System.out.println("Por favor, digite seu saldo!");
+        saldoConta = tc.nextDouble();
+        System.out.printf(
+                "Olá %s obrigado por criar uma conta em nosso banco,%n sua agencia %s, conta %d e seu saldo R$%5.2f já esta disponival para saque.",
+                nomeCliente, agencia, numero, saldoConta);
+
     }
 }
